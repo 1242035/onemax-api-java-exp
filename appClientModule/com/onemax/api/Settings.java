e4b8b18c09d8d99f84359220e7ff39e8a7457a49
Lục Tuyết Kỳ <1242035@users.noreply.github.com>
2016-11-03 12:59:42 +0700

    Update Settings.java

--- appClientModule/com/onemax/api/Settings.java
+++ appClientModule/com/onemax/api/Settings.java
@@ -3,7 +3,7 @@ package com.onemax.api;
 public class Settings 
 {
 	/** Api base url **/
-	public static final String BASE_URL = "http://api-sandbox.onemax.com.vn:1310";
+	public static final String BASE_URL = "http://api.example.com";
 	
 	/** Auth url **/
 	public static final String TOKEN_URL = BASE_URL + "/oauth/auth";
@@ -11,14 +11,12 @@ public class Settings
 	/** Auth mode **/
 	public static final String TOKEN_MODE = "otp_auth";
 	/** Device name **/
-	public static final String DEVICE_NAME = "TPBANK";
+	public static final String DEVICE_NAME = "BRAND_CODE";
 	/** Value of the "API Key". */
-	//public static final String API_KEY = "1874a7cc535cbb67b17c0002fff4a510";
-	public static final String API_KEY = "a7c89ae387f166e68291712363e27136";
+	public static final String API_KEY = "1QWEFQWEFQWEQWEQWE";
 
 	/** Value of the "API Secret". */
-	//public static final String API_SECRET = "c42183657c734c165d9a40b55944bdb3";
-	public static final String API_SECRET = "9bwBzDMX2Nscuehp8TjHSShUkM3rjUB3uV";
+	public static final String API_SECRET = "ASKASD238U128938123";
 	  
 	/** Version **/
 	public static final String API_VERSION = "1.0";
